5b521ed877b29c233df08cb464430b714343576c
BerserkrMM <mr.monopoly.od@gmail.com>
2023-12-29 23:03:16 +0200

    h2 for tests added

--- src/test/java/com/example/expand_apis_task/ExpandApisTaskApplicationTests.java
+++ src/test/java/com/example/expand_apis_task/ExpandApisTaskApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.expand_apis_task;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class ExpandApisTaskApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        mockMvc.perform(post("/user/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(" {                             "
+                                + "\"username\": \"username666\","
+                                + "\"password\" : \"password666\"   "
+                                + "}                             "))
+                .andExpect(status().is(200));
     }
 
 }
